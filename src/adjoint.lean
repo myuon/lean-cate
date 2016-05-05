9e71e61c2a09d64b17ab3145ff6dde564387950a
myuon <ioi.joi.koi.loi@gmail.com>
2016-05-05 09:41:56 +0900

    unit

--- src/adjoint.lean
+++ src/adjoint.lean
@@ -18,12 +18,31 @@ namespace adjoint
   := naturality (adj_nat adj)
 
   definition naturality_left_point {C D : category} {F : functor C D} {G : functor D C} (adj : F ⊣ G) {x y : @obj C} {a : @obj D} {f : @hom C y x} (k : @hom D (fobj F x) a) :
-  (iso.mapr (adjunction adj) (fmap (Hom[D][F-;-] f[-,a]) f k)) = (fmap (Hom[C][-;G-] f[-,a]) f (iso.mapr (adjunction adj) k))
-  := congr_fun (naturality_left adj) k
+  (iso.mapr (adjunction adj) (k ∘[D] fmap F f)) = ((iso.mapr (adjunction adj) k) ∘[C] f)
+  := calc
+    iso.mapr (adjunction adj) (k ∘[D] fmap F f) = iso.mapr (adjunction adj) (fmap (Hom[D][F-;-] f[-,a]) f k) : by unfold homLF; unfold bifunctor.right_obj; rewrite id_left
+    ... = fmap (Hom[C][-;G-] f[-,a]) f (iso.mapr (adjunction adj) k) : congr_fun !naturality_left k
+    ... = (((@id C _) ∘[C] (iso.mapr (adjunction adj) k)) ∘[C] f) : by unfold bifunctor.right_obj; unfold homRF; rewrite functor.preserve_id
+    ... = ((iso.mapr (adjunction adj) k) ∘[C] f) : proof
+      have ((@id C _) ∘[C] (iso.mapr (adjunction adj) k)) = (iso.mapr (adjunction adj) k), begin rewrite id_left, end,
+      show _, from _
+      qed
 
   definition naturality_right {C D : category} {F : functor C D} {G : functor D C} (adj : F ⊣ G) {x : @obj C} {a b : @obj D} {h : @hom D a b} :
   (component (adj_nat adj) _ ∘[Types] fmap (Hom[D][F-;-] f[x,-]) h) = (fmap (Hom[C][-;G-] f[x,-]) h ∘[Types] component (adj_nat adj) _)
   := naturality (adj_nat adj)
+
+  definition unit {C D : category} {F : functor C D} {G : functor D C} {adj : F ⊣ G} : @functor.id C ⇒ (G ∘f F) := natrans.mk
+  (λx, iso.mapr (adjunction adj) (@id D _))
+  (λa b f, calc
+    (iso.mapr (adjunction adj) (@id D _) ∘[C] fmap functor.id f) = (iso.mapr (adjunction adj) (@id D _) ∘[C] f) : rfl
+    ... = iso.mapr (adjunction adj) (@id D _ ∘[D] fmap F f) : begin
+      
+      end
+    ... = (fmap (G ∘f F) f ∘[C] iso.mapr (adjunction adj) (@id D _)) : _)
+
+-- comp (iso.mapr (adjunction adj) id) (fmap id f) = comp (fmap (G∘f F) f)
+--    (iso.mapr (adjunction adj) id)
 
 /-
   definition unit {C D : category} {F : functor C D} {G : functor D C} {adj : F ⊣ G} : @functor.id C ⇒ (G ∘f F) := natrans.mk
